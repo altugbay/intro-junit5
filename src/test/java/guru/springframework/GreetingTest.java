19b211a27ddceae1cd0d8f4aa404a2b85ee4aa2c
Altug Bayram <altugbay@gmail.com>
2021-11-09 22:43:54 +0300

    test codes

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,22 +1,39 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Test;
-
-import java.awt.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
+    private Greeting greeting;
+
+    @BeforeAll
+    static void beforeClass() {
+        System.out.println("Before all called once");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Before each...");
+        greeting = new Greeting();
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void testHelloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld("Altug"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("After each");
+    }
+
+    @AfterAll
+    static void afterClass() {
+        System.out.println("After all called once");
     }
 }
